ab2e31ce7bdcacd81278bae8373e1ae21819d8bd
IHN-GG <162330516+IHN-GG@users.noreply.github.com>
2024-03-05 17:04:31 +0200

    Update omni_sp7731e_1h10.mk

--- omni_sp7731e_1h10.mk
+++ omni_sp7731e_1h10.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from sp7731e_1h10 device
 $(call inherit-product, device/sprd/sp7731e_1h10/device.mk)
